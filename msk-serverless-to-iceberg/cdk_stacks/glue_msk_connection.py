9bb376bf6940d12a845ad8148884b0a5020e1a18
ksmin23 <sonagy23@gmail.com>
2024-09-10 21:06:18 +0900

    refine coding style

--- msk-serverless-to-iceberg/cdk_stacks/glue_msk_connection.py
+++ msk-serverless-to-iceberg/cdk_stacks/glue_msk_connection.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+# vim: tabstop=2 shiftwidth=2 softtabstop=2 expandtab
+
 import boto3
 
 import aws_cdk as cdk
@@ -43,7 +46,7 @@ class GlueMSKConnectionStack(Stack):
       "KAFKA_BOOTSTRAP_SERVERS": kafka_bootstrap_servers,
       "KAFKA_SSL_ENABLED": "false"
     }
-  
+
     subnet = vpc.select_subnets(subnet_type=aws_ec2.SubnetType.PRIVATE_WITH_EGRESS).subnets[0]
 
     connection_input_property = aws_glue.CfnConnection.ConnectionInputProperty(
